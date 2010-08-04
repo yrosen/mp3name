b5e753c91d5f9814a24291a850fdba3b653ba282
Yudi Rosen <yudi42@gmail.com>
2010-08-04 02:32:50 -0500

    Swapped os.walk for os.listdir

---
+++ mp3name.py
@@ -0,0 +1,37 @@
+# Needs id3reader: http://nedbatchelder.com/code/modules/id3reader.py
+#
+# TODO: command-line arguments:
+# * mp3name.exe [c:\dir\to\scan] [-i] [-c] [-f]
+# * Dir to scan = defaults to current dir
+# * -i = Invert artist (e.g. 'The Beatles' = 'Beatles, The'
+# * -c = Capitalize Words (default uses tag data)
+# * -f = Sort into artist/album folders
+import os, re, id3reader
+
+for file in os.listdir('.' + os.sep):
+    ext = os.path.splitext(file)
+
+    if ext[1] == '.mp3':
+        # Construct a reader from a file or filename.
+        id3r = id3reader.Reader(file)
+
+        #Artist, album, title:
+        artist = id3r.getValue('performer') + ' - ' if(len(id3r.getValue('performer')) != 0) else ''
+        album  = id3r.getValue('album')     + ' - ' if(len(id3r.getValue('album'))     != 0) else ''
+        title  = id3r.getValue('title')             if(len(id3r.getValue('title'))     != 0) else ext[0]
+
+	# Sometimes track is stored like 06/10, we just want the 06:
+        track = re.sub('/\d+', '', id3r.getValue('track'))
+
+	# TODO: Somehow get max len() of track? (e.g. 01 vs 001)
+        if len(track) == 0:
+            track = ''
+        elif len(track) == 1:
+            track = '0' + track + ' - '
+        elif len(track) == 2:
+            track = track + ' - '
+
+        filename = re.sub('[\\/:\*\?"<>\|]', '', artist + album + track + title + '.mp3')
+
+        print file, ' -> ', filename	#print filename
+        os.rename(file, filename)
